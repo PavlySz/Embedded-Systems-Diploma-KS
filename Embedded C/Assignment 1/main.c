ff84272846a4c76da8f12da68c3f32ed05734e91
PaulBlxck <paul_sz96@yahoo.com>
2020-08-20 21:51:25 +0200

    Embedded C

---
+++ Embedded C/Assignment 1/main.c
@@ -0,0 +1,11 @@
+/*
+* Embedded System Diploma by Eng. Kerolos Khalil
+* First assignment - Hello world
+* Author: Pavly Salah
+*/
+#include<stdio.h>
+
+// Main function
+void main() {
+    printf("Hello world!");
+}
---
+++ Embedded C/README.md
@@ -0,0 +1,7 @@
+### Embedded System Diploma by Eng. Kerolos Karam
+## First section - Embedded C
+
+## Author: Pavly Salah
+
+In this section we will work on some C applications.
+
